f06c01d32f592a97c59a8f899b63e37b36f7a83e
Adriano <72881543+adrianoggm@users.noreply.github.com>
2024-09-25 15:08:04 +0200

    Creo la tabla de penas
    Creo la primera tabla de la bd de datos #15

--- sqlite.py
+++ sqlite.py
@@ -1,3 +1,17 @@
 import sqlite3
 
 conn=sqlite3.connect("Gestion_Penas.db")
+c=conn.cursor()
+c.execute(""" CREATE TABLE IF NOT EXISTS PENA (
+          Idpena TEXT PRIMARY KEY,
+          Nombre TEXT,
+          Admin  TEXT NOT NULL
+          )""")
+
+#c.execute("INSERT INTO PENA VALUES ('PENGR-001','Generalife','Genadmin')")
+
+conn.commit()
+c.execute("SELECT * FROM PENA ")
+penas=c.fetchall()
+print(penas)
+conn.close()
