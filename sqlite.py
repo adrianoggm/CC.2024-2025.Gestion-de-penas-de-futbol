64cb65af592a9651e5f52ee2ac39c28976b004d3
Adriano <72881543+adrianoggm@users.noreply.github.com>
2024-09-25 16:13:10 +0200

    Arreglado problema de la foreign key
    #15  mediante PRAGMA

--- sqlite.py
+++ sqlite.py
@@ -2,30 +2,32 @@ import sqlite3
 
 conn=sqlite3.connect("Gestion_Penas.db")
 c=conn.cursor()
+c.execute("PRAGMA foreign_keys = ON;")
 c.execute(""" CREATE TABLE IF NOT EXISTS PENA(
-          Idpena TEXT PRIMARY KEY,
+          Idpena INTEGER PRIMARY KEY AUTOINCREMENT,
           Nombre TEXT,
           Admin  TEXT NOT NULL
           )""")
 c.execute(""" CREATE TABLE IF NOT EXISTS JUGADOR(
-          Idjugador TEXT PRIMARY KEY,
+          Idjugador INTEGER PRIMARY KEY AUTOINCREMENT,
           Nombre TEXT,
           Apellidos TEXT,
           Nacionalidad TEXT
           )""")
 c.execute(""" CREATE TABLE IF NOT EXISTS JUGADORPENA (
-          Idjugadorp TEXT,
-          Idjugador TEXT,
-          Idpena TEXT,
+          Idjugador INTEGER,
+          Idpena INTEGER,
           Mote TEXT,
           Posicion TEXT,
-          PRIMARY KEY (Idjugadorp,Idjugador,Idpena),
-          FOREIGN KEY (Idjugador) REFERENCES JUGADOR(Idjugador),
-          FOREIGN KEY (Idpena) REFERENCES PENA(Idpena)
+          PRIMARY KEY (Idjugador,Idpena),
+          FOREIGN KEY (Idjugador) REFERENCES JUGADOR(Idjugador) ON UPDATE CASCADE ON DELETE CASCADE,
+          FOREIGN KEY (Idpena) REFERENCES PENA(Idpena) ON UPDATE CASCADE ON DELETE CASCADE
           )""")
-#c.execute("INSERT INTO PENA VALUES ('PENGR-001','Generalife','Genadmin')")
-#c.execute("INSERT INTO JUGADOR VALUES ('PENGEN-001','Adriano','García-Giralda Milena','ESPAÑOLA')")
-#c.execute("INSERT INTO JUGADORPENA VALUES ('JPGEN-001','PENGEN-001','PENGR-001','Adri jr','ATACANTE')")
+#c.execute("INSERT INTO PENA (Nombre,Admin) VALUES ('Generalife','Genadmin')")
+#c.execute("INSERT INTO PENA VALUES ('0','Generalife','Genadmin')")
+#c.execute("INSERT INTO PENA (Nombre,Admin) VALUES ('Churriana','CHadmin')")
+#c.execute("INSERT INTO JUGADOR(Nombre,Apellidos,Nacionalidad) VALUES ('Adriano','García-Giralda Milena','ESPAÑOLA')")
+#c.execute("INSERT INTO JUGADORPENA VALUES ('1','1','Adri jr','ATACANTE')")
 conn.commit()
 c.execute("SELECT * FROM JUGADORPENA ")
 penas=c.fetchall()
