ab4bb3fdb851e45ba8ffacb4020e22127f4ea2c8
Adriano <72881543+adrianoggm@users.noreply.github.com>
2024-09-25 14:23:21 +0200

    Creamos la primera web funcionando
    Creamos primeros ficheros de BD y app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify
+from users import users 
+
+app =Flask(__name__)
+@app.route('/',methods=['GET'])
+
+def ping():
+    return jsonify({"response ":"Hello world"})
+
+@app.route('/users')
+def usersHandler():
+     return jsonify({"users ":users})
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=4000,debug=True)
+
