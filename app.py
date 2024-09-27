9aae9ac0f362c00ff2080e09289bf065c38d3562
Adriano <72881543+adrianoggm@users.noreply.github.com>
2024-09-27 11:28:09 +0200

    Creo la primera interfaz y logging
    #9 Avanzo  para conseguir log y registro de un admin

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask,jsonify
+from flask import Flask, flash,jsonify, render_template, request, make_response,render_template_string, redirect, url_for
 from users import users 
 
 app =Flask(__name__)
+app.secret_key = 'supersecretkey'
 @app.route('/',methods=['GET'])
 
 def ping():
@@ -10,6 +11,34 @@ def ping():
 @app.route('/users')
 def usersHandler():
      return jsonify({"users ":users})
+
+# Formulario de login HTML como plantilla dentro del código
+
+# Ruta de login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    
+    
+    if request.method == 'POST':
+        # Recibir las credenciales del formulario (formulario POST)
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Verificar si el usuario existe y si la contraseña es correcta
+        if username in users and users[username]["password"] == password:
+            # Redirigir al usuario a una página de éxito después del login
+            return f"¡Bienvenido, {users[username]['name']}!"
+        else:
+            flash('Usuario o contraseña incorrectos. Inténtalo de nuevo.')
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+
+# Manejo de errores personalizados
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "No encontrado"}), 404)
+
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=4000,debug=True)
 
